65f07238f7339d6c8a9891f9444e32ecff25fa6d
Survine <hridaybardhan18@gmail.com>
2025-04-02 12:58:37 +0530

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,6 +17,9 @@ const Projects = () => {
             <div className='w-full max-w-xl lg:w-3/4'>
               <h3 className='mb-2 font-semibold text-2xl'>{project.title}</h3>
               <p className='mb-4 text-stone-400'>{project.description}</p>
+              {project.technologies.map((tech, index) => (
+                <span className='mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300' key={index}>{tech}</span>
+              ))}
             </div>
           </div>
         ))}
